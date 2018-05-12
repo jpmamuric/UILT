78bc21605a8d64235fcaa1a891505b85929dc17e
Jan Peter Mamuric <webdev.jmamuric@gmail.com>
2018-05-12 11:55:20 -0700

    added mongoose and routing

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
-const index = require('./routes/index');
-const app = express();
+const mongoose = require('mongoose');
+
+const keys = require('./config/keys');
 const PORT = process.env.PORT || '5000';
+const app = express();
+
+const index = require('./routes/index');
+
+mongoose.connect(keys.mongoURI);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongoose connection error:'));
+db.once('open', () => console.log('mongoose is connected'));
 
 
 app.use('/api', index);
